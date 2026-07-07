16a4499f1fce2e6dcdca257b9fbb7f0c5f1737db
dev-addhayyan <addhayyanyt@gmail.com>
2026-07-08 03:14:17 +0530

    comparision

---
+++ basics/comparision.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+if a>b:
+    print("The first number is greater than the second number")
+elif a<b:
+    print("The second number is greater than the first number")
+else:
+    print("Both numbers are equal")
