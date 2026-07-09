3ea49bd36a58e8540d34e81e5c56653942af1c9d
dev-addhayyan <addhayyanyt@gmail.com>
2026-07-09 23:17:53 +0530

    TURNING 18 CALCULATOR

---
+++ basics/telling when the user is gonna be turning 18.py
@@ -0,0 +1,8 @@
+x=input("Enter Your Name: ")
+#asking user to enter their Name
+y=int(input("Enter Your Age: "))
+#asking user to enter their Age
+if y>18:
+    print("You are already 18 or older")
+else:
+    print("You will be turning 18 in",18-y,"years.")
