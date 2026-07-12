408b69e7baa26638fd244bc779773ae6670ef09d
dev-addhayyan <addhayyanyt@gmail.com>
2026-07-13 04:58:08 +0530

    made the password more secure by adding conditions to check for the presence of '@' and '.' in the password.

--- basics/seeting the lenght of password.py
+++ basics/seeting the lenght of password.py
@@ -1,8 +1,6 @@
 x=input("Set Password : ")
 #asking user to set password
-if len(x) >= 8:
-    print("Password is valid")
-elif "@" in x and "." in x:
+if len(x) >= 8 and "@" in x and "." in x:
     print("Password is valid")
 else:
     print("Password is not valid. Password should be at least 8 characters long and contain '@' and '.')
